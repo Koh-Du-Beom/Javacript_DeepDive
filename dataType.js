df768d0c5dbf3efe060b7984b342efc874670e6f
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2023-11-14 00:50:43 +0900

    first commit

---
+++ dataType.js
@@ -0,0 +1,13 @@
+var binary = 0b01000001;
+var octal = 0o101;
+var hex = 0x41;
+
+console.log(binary); //65
+console.log(octal); //65
+console.log(hex); //65
+console.log(binary === hex); //true
+console.log(octal === hex); //true
+
+var num1 = 10;
+var num2 = 20;
+console.log(`${num1} + ${num2} = ${num1 + num2}입니다.`);
---
+++ variable.js
@@ -0,0 +1,5 @@
+console.log(score);
+var score;
+score=80;
+console.log(score);
+
