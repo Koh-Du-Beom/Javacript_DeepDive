2a879f45c4b835677f46d0864dea7ce273dc714e
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-07 18:44:14 +0900

    page 503 chapter27

--- codes/array.js
+++ codes/array.js
@@ -7,5 +7,23 @@ console.log([1,2,3].length);
 
 const arr2 = [1];
 arr2.length = 3;
-console.log(arr2)
+console.log(arr2);
 
+const sparse = [, 2, , 4];
+console.log(sparse.length);
+console.log(sparse);
+console.log(Object.getOwnPropertyDescriptors(sparse));
+
+const arr3 = [1,2,3];
+const arr4 = new Array(10);
+console.log(arr4);
+console.log(arr4.length);
+
+const arr5 = Array.of(1,2,3);
+console.log(arr5);
+
+const arr6 = Array.from({ length : 2, 0: 'a', 1: 'b'});
+console.log(arr6);
+
+const arr7 = Array.from('Hello');
+console.log(arr7);
