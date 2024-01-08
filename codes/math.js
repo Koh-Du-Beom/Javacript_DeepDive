bde26aa84e23a529625ee34aa91f8546ca5291ae
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-08 17:27:10 +0900

    chapter 30 done

--- codes/math.js
+++ codes/math.js
@@ -2,5 +2,10 @@ console.log(Math.PI);
 console.log(Math.abs(-1));
 console.log(Math.round(1.234));
 console.log(Math.ceil(1.4));
-console.log(MAth.floor(-1.56));
-console.log(Math.sqrt())
+console.log(Math.floor(-1.56));
+
+const array = [1,2,3,4,5];
+const max = Math.max(...array);
+const min = Math.min(...array);
+
+console.log(max, min);
---
+++ codes/read_0/date.js
@@ -0,0 +1,5 @@
+console.log(new Date());
+console.log(new Date(324324315132));
+
+const now = Date.now();
+console.log(now);
